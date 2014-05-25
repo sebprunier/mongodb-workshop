09e6b269db95c8a0822ff8a753bb42c8ec271212
sebprunier <sebastien.prunier@gmail.com>
2014-05-25 22:17:05 +0200

    Upgrade Gruntfile

--- angular-js/Gruntfile.js
+++ angular-js/Gruntfile.js
@@ -2,13 +2,26 @@ module.exports = function (grunt) {
 
     // Project configuration.
     grunt.initConfig({
+        // Directories
+        reportsDir: 'reports',
+        // delete files and folders
+        clean: ['<%= reportsDir %>'],
+        // JSHint
         jshint: {
-            all: ['app/app.js', 'app/controller/*.js', 'app/filters/*.js']
+            all: ['app/app.js', 'app/controller/*.js', 'app/filters/*.js'],
+            options: {
+                reporter: 'checkstyle',
+                reporterOutput: '<%= reportsDir %>/checkstyle.xml',
+                force: true,
+                globals: {
+                    angular: true
+                }
+            }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-jshint');
 
-
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['clean', 'jshint']);
 };
